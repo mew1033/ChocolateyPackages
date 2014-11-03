ba84e4e4dc3d0823d6976d5dd1221cad7eec4f6e
mew1033 <chandler.newby@gmail.com>
2014-11-03 14:10:45 -0700

    Initial Tera Term Commit

---
+++ TeraTerm/tools/chocolateyInstall.ps1
@@ -0,0 +1,7 @@
+﻿$packageName = 'TeraTerm'
+$installerType = 'EXE' #only one of these: exe, msi, msu
+$url = 'http://en.sourceforge.jp/frs/redir.php?m=iij&f=%2Fttssh2%2F61906%2Fteraterm-4.84.exe'
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
